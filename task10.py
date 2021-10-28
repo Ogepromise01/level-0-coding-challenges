18df568fcbe4894ae240aea299ffec6b1445764d
Ogepromise01 <ogepromise019@gmail.com>
2021-10-28 02:37:19 +0200

    common letters

---
+++ task10.py
@@ -0,0 +1,8 @@
+NO 10
+def common_letters(str1, str2):
+    s1 = set(str1)
+    s2 = set(str2)
+    com_let = s1 & s2 #intersection of str1 and str2
+    print("Common Letters:", com_let)
+
+common_letters("house", "computers")
